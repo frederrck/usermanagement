bb7d347751e070989c021119946013f4443b3ffe
frederrck <fremu@student.pakuranga.school.nz>
2023-07-06 18:30:23 +1200

    5/07/2023

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import pymysql
 import hashlib
 import uuid
 import os
+import time
+from jinja2 import Environment
 from flask import Flask, render_template, request, redirect, session, flash
 app = Flask(__name__)
 
@@ -10,14 +12,15 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 app.secret_key = "any-random-string-reshrdjtfkygluvchfjkhlbh"
 
 
+
 def create_connection():
     return pymysql.connect(
-        host="10.0.0.17",
-        user="fremu",
-        # host="127.0.0.1",
-        # user="root",
-        password="ARENA",
-        db="fremu_test",
+        # host="10.0.0.17",
+        # user="fremu",
+        host="127.0.0.1",
+        user="root",
+        password=".magnesiumOxide123",
+        db="user_management",
         charset="utf8mb4",
         cursorclass=pymysql.cursors.DictCursor
     )
@@ -74,12 +77,14 @@ def view_profile():
 
 @app.route("/feed")
 def feed():
+    env = Environment()
+    env.filters['reversed'] = reversed  
     with create_connection() as connection:
         with connection.cursor() as cursor:
             sql = """SELECT * FROM posts 
-                    LEFT JOIN users ON posts.user_id = users.id 
+                    LEFT JOIN users ON posts.user_id = users.id WHERE id != %s
                 """
-            cursor.execute(sql)
+            cursor.execute(sql, session["id"])
             result = cursor.fetchall()
     return render_template("feed.html", result=result)
 
@@ -98,7 +103,7 @@ def my_posts():
             }
 
             cursor.execute(sql, values)
-            result = cursor.fetchall()
+            result = cursor.fetchall()    
     return render_template("my_posts.html", result=result)
 
 @app.route("/post/add", methods = ["GET", "POST"])
@@ -116,11 +121,22 @@ def add_post():
                 else:
                     audio_path = None
 
-                sql = "INSERT INTO posts (content, audio, genre, user_id) VALUES (%s, %s, %s, %s)"
-                values = (request.form['content'], audio_path, request.form["genre"],session["id"])
+                image = request.files["cover_img"]
+                if image:
+                    # Choose a random filename to prevent clashes
+                    ext = os.path.splitext(image.filename)[1]
+                    image_path = "static/images/" + str(uuid.uuid4())[:8] + ext
+                    image.save(image_path)
+                else:
+                    image_path = None
+
+                sql = "INSERT INTO posts (content, audio, genre, cover_img, user_id) VALUES (%s, %s, %s, %s, %s)"
+                values = (request.form['content'], audio_path, request.form["genre"], image_path , session["id"])
                 cursor.execute(sql, values)
                 connection.commit()
+                flash("Posted")
         return render_template("post_add.html") 
+
     else:
         return render_template("post_add.html")  
 
@@ -262,30 +278,81 @@ def update():
 
 
 # DELETE
-#/update?id=1
 @app.route("/delete")
 def delete():
-    if not can_access(request.args["id"]):
-        flash("You don't have permission to do that")
-        return redirect('/')
+
+    # if not can_access(request.args["id"]):
+    #     flash("You don't have permission to do that")
+    #     return redirect('/')
 
     with create_connection() as connection:
-            with connection.cursor() as cursor: 
-                # Get the image path before deleting the user
-                sql = """SELECT * FROM posts 
-                    LEFT JOIN users ON posts.user_id = users.id WHERE users.id = %s"""
-                values = (request.args["id"])
-                cursor.execute(sql, values)
-                result = cursor.fetchone()
-                if result["audio"]:
-                    os.remove(result["audio"])
+        with connection.cursor() as cursor: 
+            # Get the audio path before deleting the user
+            sql = """SELECT * FROM posts 
+                LEFT JOIN users ON posts.user_id = users.id WHERE users.id = %s"""
+            values = (request.args["id"])
+            cursor.execute(sql, values)
+            result = cursor.fetchone()
+            if "audio" in request.args and request.args["audio"] is not None and request.args["audio"]:
+                audio_file = request.args["audio"]
+                if os.path.exists(audio_file):
+                    os.remove(audio_file)
+            else:
+                pass
+            
 
-                sql = "DELETE FROM posts WHERE post.id = %s"
-                values = (request.args["id"])
+            sql = "DELETE FROM posts WHERE post_id = %s"
+            values = (request.args["id"])
+            cursor.execute(sql, values)
+            connection.commit()
+            flash("Post has been deleted","success")
+    return redirect("/my_posts")
+
+
+# Update Own Posts. 
+@app.route("/my_posts/edit", methods = ["GET", "POST"])
+def updatepost(): 
+    # if not can_access(request.args["id"]):
+    #     flash("You don't have permission to do that")
+    #     return redirect('/')
+
+    if request.method == "POST":
+        with create_connection() as connection:
+            with connection.cursor() as cursor:
+
+                image = request.files["image"]
+
+                if image:
+                    ext = os.path.splitext(image.filename)[1]
+                    image_path = "static/images/" + str(uuid.uuid4())[:8] + ext
+                    image.save(image_path)
+                    if request.form["old_image"]:
+                        os.remove(request.form["old_image"])
+                else:
+                    image_path = request.form["old_image"]
+
+
+                sql = """UPDATE posts SET 
+                    content = %s,
+                    cover_img = %s,
+                    WHERE id = %s
+                """
+                values = (
+                    request.form[''],
+                    request.form['last_name'],
+                    request.form['id']
+                )
                 cursor.execute(sql, values)
                 connection.commit()
-    return redirect("/")
-
+        return redirect("/")
+    else:
+        with create_connection() as connection:
+            with connection.cursor() as cursor:
+                sql = "SELECT * FROM users where id = %s"
+                values = (session["id"])
+                cursor.execute(sql, values)
+                result = cursor.fetchone()
+        return render_template("editpost.html", result=result)
 
 # /admin?id=1&role=admin
 @app.route("/admin")
@@ -304,13 +371,75 @@ def toggle_admin():
         flash("You don't have permission to do that!")
     return redirect("/")   
 
-#like and dislike
-# def add_like():
-#     with create_connection() as connection:
-#         with connection.cursor() as cursor:
-#             if "liked" in 
+def user_liked(user_id, post_id):
+    connection = create_connection()
+    
+    try:
+        with connection.cursor() as cursor:
+            sql = "SELECT * FROM likes WHERE user_id = %s AND post_id = %s"
+            cursor.execute(sql, (user_id, post_id))
+            result = cursor.fetchone()
+
+            if result:
+                return True
+            else: 
+                return False
+    finally:
+        connection.close()
+
+def add_like(user_id, post_id):
+    connection = create_connection()
+
+    try:
+        with connection.cursor() as cursor:
+            sql = "INSERT INTO likes (user_id, post_id) VALUES (%s, %s)"
+            cursor.execute(sql, (user_id, post_id))
+        
+        connection.commit()
+    finally:
+        connection.close()
+
+# updating likes count 
+def update_likes(post_id):
+    connection = create_connection()
+
+    try:
+        with connection.cursor() as cursor:
+            likes_sql = "SELECT COUNT(*) FROM likes WHERE post_id = %s"
+            cursor.execute(likes_sql, (post_id,))
+            result = cursor.fetchone()
+
+            if result is not None and len(result) > 0:
+                count = result[0]
+            else:
+                count = 0
+
+            update_sql = "UPDATE posts SET likes_count = %s WHERE post_id = %s"
+            cursor.execute(update_sql, (count, post_id))
+
+        connection.commit()
+    # except:
+    #     pass
+    finally:
+        connection.close()
 
 
+
+# post likes and dislikes
+@app.route("/feed/like", methods=["POST"])
+def like_post():
+    user_id = session["id"]
+    post_id = request.args["post_id"] 
+
+    if user_liked(user_id, post_id):
+        flash("Already Liked")
+        return redirect("/feed")
+    
+    add_like(user_id, post_id)
+
+    update_likes(post_id)
+
+    return redirect('/feed')
 
 
 # Check Email
